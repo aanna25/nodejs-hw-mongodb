1e448df268b0c9e7b902eea7a9f98f360b4fafe7
Hanna Ponomarenko <pnmrnk.anna@gmail.com>
2025-09-11 16:46:07 +0200

    add hw7

--- src/server.js
+++ src/server.js
@@ -8,6 +8,7 @@ import router from './routers/index.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import cookieParser from 'cookie-parser';
 import { UPLOAD_DIR } from './constants/index.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 dotenv.config();
 
@@ -21,10 +22,10 @@ export const setupServer = () => {
 
   app.use(router);
 
+  app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs());
   app.use(notFoundHandler);
   app.use(errorHandler);
-
-  app.use('/uploads', express.static(UPLOAD_DIR));
 
   const port = Number(process.env.PORT) || 3000;
 
